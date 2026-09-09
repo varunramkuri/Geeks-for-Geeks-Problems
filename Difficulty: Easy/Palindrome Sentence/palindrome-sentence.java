ad2b35ec9cb801991a68d8e136df5157ac4b41c6
varun <varunramkuri23@gmail.com>
2026-09-09 21:48:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Palindrome Sentence/palindrome-sentence.java
+++ Difficulty: Easy/Palindrome Sentence/palindrome-sentence.java
@@ -1,20 +1,21 @@
 class Solution {
     public boolean isPalinSent(String s) {
-        // code here
+
         s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
 
-                int left = 0;
-                int right = s.length() - 1;
+        int left = 0;
+        int right = s.length() - 1;
 
-                while (left < right) {
-                    if (s.charAt(left) != s.charAt(right)) {
-                        return false;
-                    }
+        while (left < right) {
 
-                    left++;
-                    right--;
-                }
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
 
-                return true;
+            left++;
+            right--;
+        }
+
+        return true;
     }
 }
